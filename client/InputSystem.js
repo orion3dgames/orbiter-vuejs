4fa64a2643c200dff4ab5560d0ed78f3044aa7d1
DavidFM <faivred@gmail.com>
2022-03-25 19:10:00 +0100

    just a try

--- client/InputSystem.js
+++ client/InputSystem.js
@@ -101,34 +101,38 @@ class InputSystem {
     })
 
     const ctx = this;
-    window.AFRAME.registerComponent('thumbstick-logging', {
-      init: function () {
-        this.el.addEventListener('thumbstickmoved', this.logThumbstick);
-      },
-      logThumbstick: function (evt) {
-        if (evt.detail.y > 0.95) {
-          console.log("DOWN");
-          ctx.debug('DOWN')
-          ctx.frameState.s = true
+    setTimeout(() => {
+      ctx.debug("registerComponent('thumbstick-logging'")
+      console.log("registerComponent('thumbstick-logging'")
+      window.AFRAME.registerComponent('thumbstick-logging', {
+        init: function () {
+          this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+        },
+        logThumbstick: function (evt) {
+          if (evt.detail.y > 0.95) {
+            console.log("DOWN");
+            ctx.debug('DOWN')
+            ctx.frameState.s = true
+          }
+          if (evt.detail.y < -0.95) {
+            console.log("UP");
+            ctx.debug('UP')
+            window.location.reload()
+            ctx.frameState.w = true
+          }
+          if (evt.detail.x < -0.95) {
+            console.log("LEFT");
+            ctx.debug('LEFT')
+            ctx.frameState.a = true
+          }
+          if (evt.detail.x > 0.95) {
+            console.log("RIGHT");
+            ctx.debug('RIGHT')
+            ctx.frameState.d = true
+          }
         }
-        if (evt.detail.y < -0.95) {
-          console.log("UP");
-          ctx.debug('UP')
-          window.location.reload()
-          ctx.frameState.w = true
-        }
-        if (evt.detail.x < -0.95) {
-          console.log("LEFT");
-          ctx.debug('LEFT')
-          ctx.frameState.a = true
-        }
-        if (evt.detail.x > 0.95) {
-          console.log("RIGHT");
-          ctx.debug('RIGHT')
-          ctx.frameState.d = true
-        }
-      }
-    });
+      });
+    }, 5000);
   }
 
   releaseKeys() {
--- client/graphics/AFrame.js
+++ client/graphics/AFrame.js
@@ -127,7 +127,7 @@ class AFRAMERenderer {
       }
 
       this.sceneEl.appendChild(entityEl);
-
+      console.log("entity added")
     }
   }
 
