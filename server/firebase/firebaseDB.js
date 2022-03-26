9698f87804e1c8f4052ffa535c9a0fd1692136b1
Orion Gunning <orion.gunning@gmail.com>
2022-03-26 13:43:49 +1100

    Update firebaseDB.js

--- server/firebase/firebaseDB.js
+++ server/firebase/firebaseDB.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 admin.initializeApp({
   credential: admin.credential.cert({
     "projectId": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
   project_id: process.env.FIREBASE_PROJECT_ID,
