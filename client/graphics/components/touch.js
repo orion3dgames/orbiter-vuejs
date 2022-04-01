06e8c0eadea4567e974714377ee74ea973fb276d
DavidFM <faivred@gmail.com>
2022-04-02 01:11:54 +0200

    fix touch move

--- client/graphics/components/touch.js
+++ client/graphics/components/touch.js
@@ -5,9 +5,9 @@ window.AFRAME.registerComponent('thumbstick-logging', {
     },
     logThumbstick: function (e) {
         const limite = 0.1;
-        window.app.gameClient.input.keyState.up = e.detail.y < -limite ? e.detail.y : 0;
+        window.app.gameClient.input.keyState.up = e.detail.y < -limite ? -e.detail.y : 0;
         window.app.gameClient.input.keyState.down = e.detail.y > limite ? e.detail.y : 0;
-        window.app.gameClient.input.keyState.left = e.detail.x < -limite ? e.detail.x : 0;
+        window.app.gameClient.input.keyState.left = e.detail.x < -limite ? -e.detail.x : 0;
         window.app.gameClient.input.keyState.right = e.detail.x > limite ? e.detail.x : 0;
 
         if (e.detail.y > limite) {
