61ec4da462861bcb88a5a26b31e39c08f8c5c814
Orion Gunning <orion.gunning@gmail.com>
2022-04-04 17:13:31 +1000

    adding cube should be working better now
    - the issue was you need to do a deep copy else you juts modifying the position of the target cube

--- client/graphics/components/intersection-spawn.js
+++ client/graphics/components/intersection-spawn.js
@@ -6,31 +6,51 @@ window.AFRAME.registerComponent('intersection-spawn', {
     parse: window.AFRAME.utils.styleParser.parse
   },
   init: function () {
+
     const cursor = this.el;
     let intersected = null;
     let down = false;
-    let point = {};
+    let adjustedPoint = {};
+    let intersectedType = null;
+
     cursor.addEventListener('mousedown', e => {
+
       down = new Date().getTime();
-      intersected = e.detail.intersectedEl
-      const faceIndex = e.detail.intersection.face?.materialIndex;
-      point = e.detail.intersectedEl.object3D.position
-      if (intersected.getAttribute('type') === 'standard') {
-        if (faceIndex === 2) point.y += 1 // top
-        if (faceIndex === 1) point.x -= 1 // WEST
-        if (faceIndex === 0) point.x += 1// EST
-        if (faceIndex === 4) point.z += 1// SOUTH
-        if (faceIndex === 5) point.z -= 1 // NORTH
+      intersected = e.detail.intersectedEl;
+      intersectedType = intersected.getAttribute('type');
+      let faceIndex = e.detail.intersection.face?.materialIndex;
+
+      let currentPoint = Object.assign({}, intersected.object3D.position); // save original ray point just in case
+      adjustedPoint = Object.assign({}, currentPoint); // do a real copy
+
+      //
+      if(intersectedType === 'standard') {
+        if (faceIndex === 2) adjustedPoint.y += 1 // top
+        if (faceIndex === 1) adjustedPoint.x -= 1 // WEST
+        if (faceIndex === 0) adjustedPoint.x += 1// EST
+        if (faceIndex === 4) adjustedPoint.z += 1// SOUTH
+        if (faceIndex === 5) adjustedPoint.z -= 1 // NORTH
       }
-      if (intersected.getAttribute('type') === 'crate') {
-        point.y += 1
+
+      // ????
+      if (intersectedType === 'crate') {
+        adjustedPoint.y += 1
       }
+
     });
+
     cursor.addEventListener('mouseup', e => {
+
       const keyState = window.app.gameClient.input.keyState;
+
+      // only add a cube if left click
       if (keyState.mouseType !== 'left') return;
-      if (new Date().getTime() - down < 200)
-        cursor.emit('cube_added', { x: point.x, y: point.y, z: point.z })
+
+      if (new Date().getTime() - down < 200){
+        cursor.emit('cube_added', { x: adjustedPoint.x, y: adjustedPoint.y, z: adjustedPoint.z })
+      }
+
     });
+
   }
 });
